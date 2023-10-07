e80d162439d8167cbe6adfa0b1df3a120eebf522
Jonah Kroll <krolljl@mlc-wels.edu>
2023-10-06 21:08:30 -0400

    updated a welcome greeting

--- LoadingPage/script.js
+++ LoadingPage/script.js
@@ -1,23 +1,43 @@
 document.addEventListener("DOMContentLoaded", function () {
   const progress = document.getElementById("progress");
   const startAppBtn = document.getElementById("start-app-btn");
+  const typewriterText = document.getElementById("typewriter-text");
 
-  // Simulate loading with a progress bar
-  let percentage = 0;
-  const intervalDuration = 70; // Duration in milliseconds
-  const totalTime = 7000; // Total time in milliseconds (5 seconds)
-  const steps = totalTime / intervalDuration;
+  const textToType = "Welcome to the Starbucks Randomizer App";
+  let charIndex = 0;
 
-  const interval = setInterval(function () {
-    percentage += 100 / steps;
-    progress.style.width = percentage + "%";
-    progress.textContent = Math.round(percentage) + "%"; // Display the percentage text
-
-    if (percentage >= 100) {
-      clearInterval(interval);
-      startAppBtn.classList.remove("hidden");
+  function typeText() {
+    if (charIndex < textToType.length) {
+      typewriterText.textContent += textToType.charAt(charIndex);
+      charIndex++;
+      setTimeout(typeText, 100); // Adjust typing speed by changing the timeout duration
+    } else {
+      typewriterText.style.fontWeight = "bold"; // Optionally, make the text bold
+      // When typing is complete, start the loading animation
+      startLoadingAnimation();
     }
-  }, intervalDuration);
+  }
+
+  function startLoadingAnimation() {
+    let percentage = 0;
+    const intervalDuration = 70; // Duration in milliseconds
+    const totalTime = 7000; // Total time in milliseconds (5 seconds)
+    const steps = totalTime / intervalDuration;
+
+    const interval = setInterval(function () {
+      percentage += 100 / steps;
+      progress.style.width = percentage + "%";
+      progress.textContent = Math.round(percentage) + "%"; // Display the percentage text
+
+      if (percentage >= 100) {
+        clearInterval(interval);
+        startAppBtn.classList.remove("hidden");
+      }
+    }, intervalDuration);
+  }
+
+  // Start the typewriter effect
+  typeText();
 
   // Button click event to start the Starbucks app
   startAppBtn.addEventListener("click", function () {
@@ -25,5 +45,3 @@ document.addEventListener("DOMContentLoaded", function () {
     window.location.href = "/App-page/starbucks-app.html"; // Replace with your actual Starbucks app URL
   });
 });
-
-  
