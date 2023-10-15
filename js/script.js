a1366f8ef2fed164e58c1fb9065f1766ddf89320
Jonah Kroll <krolljl@mlc-wels.edu>
2023-10-15 16:27:26 -0400

    minor update

--- js/script.js
+++ js/script.js
@@ -50,7 +50,7 @@ document.getElementById("drink-btn").addEventListener("click", function () {
 
 
 function resetOrder() {
-  document.body.style.backgroundImage = 'url(/assets/images/app-icon.png)';
+  document.body.style.backgroundImage = 'url(/assets/images/app-icon2.png)';
   clearOrderElements();
   hideAllButtons();
   document.getElementById("cold-btn").style.display = "none"; // Hide the cold button
