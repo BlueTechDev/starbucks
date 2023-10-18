0cac49933eb5077c914a7a66980ba342d54c51ae
Jonah Kroll <krolljl@mlc-wels.edu>
2023-10-18 11:22:22 -0400

    add error catching

--- js/script.js
+++ js/script.js
@@ -1,3 +1,28 @@
+
+import { tryLoadAndStartRecorder } from '@alwaysmeticulous/recorder-loader'
+
+async function startApp() {
+    // Record all sessions on localhost, staging stacks and preview URLs
+    if (!isProduction()) {
+      // Start the Meticulous recorder before you initialise your app.
+      // Note: all errors are caught and logged, so no need to surround with try/catch
+      await tryLoadAndStartRecorder({
+        projectId: '8gWMq19sbjaGngNyHGFMTEZkJBS2LWAtFwFrfQid',
+        isProduction: false,
+      });
+    }
+
+    // Initalise app after the Meticulous recorder is ready, e.g.
+    ReactDOM.render(component, document.getElementById('root'));
+}
+
+function isProduction() {
+    // TODO: Update me with your production hostname
+    return window.location.hostname.indexOf("https://starbucks-randomizer.vercel.app/") > -1;
+}
+
+startApp();
+
 let selectedSize = "";
 let milkChoice = "";
 
